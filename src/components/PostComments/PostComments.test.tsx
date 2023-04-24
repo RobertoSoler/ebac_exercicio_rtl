7ef23de4712c74635f584b142cef464b7439e1dc
Roberto Soler <procsoler@gmail.com>
2023-04-24 11:03:39 -0300

    Update PostComments.test.tsx

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,29 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve adicionar 2 comentários', () => {
+        render(<PostComment/>);
+
+        const botao = screen.getByTestId('btn-comment');
+        const comentario = screen.getByTestId('txt-comment')
+
+        fireEvent.change(comentario, {
+            target: {
+                value: 'Primeiro Comentário',
+            }
+        });
+        fireEvent.click(botao);
+    
+        fireEvent.change(comentario, {
+            target: {
+                value: 'Segundo Comentário',
+            }
+        });
+        fireEvent.click(botao);
+
+        const resultado = screen.getAllByTestId('item-comment');
+        expect(resultado).toHaveLength(2);
+    });
+
 });
